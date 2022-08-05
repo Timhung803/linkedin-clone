570199d3347b3991b5361ed7bae32abdc327daa8
Timhung803 <timhkt@gmail.com>
2022-08-06 03:08:42 +1200

    Fixed error code and created redirect after signin.

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,6 @@
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
-
 import thunkMiddleware from "redux-thunk";
 import rootReducer from "../reducers";
-// import { configureStore } from "@reduxjs/toolkit";
 
 const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
